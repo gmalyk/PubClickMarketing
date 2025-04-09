4f78b216b5f58665ce10905e33c03bd9a1204334
SpiderMan6699 <gregmalyk@gmail.com>
2025-04-09 14:19:14 +0200

    CGUCGV

--- src/screens/Terms/index.js
+++ src/screens/Terms/index.js
@@ -1,19 +1,10 @@
 import React from "react"; 
- 
-import Banner from "components/Landpage/Banner";
-import HowWorks from 'components/Landpage/HowWorks'
-import MoreInfos from 'components/Landpage/MoreInfos'
-import CommomCases from 'components/Landpage/CommomCases'
-import KnowMore from 'components/Landpage/KnowMore'
-import LittleMore from 'components/Landpage/LittleMore' 
-
 import ContainerLandpage from "containers/Landpage";
 import { Col, Container, Row } from "reactstrap";
 import { PageContent } from "./styled";
 import useI18n from "hooks/useI18n";
 
 export default function Terms(){ 
-
     const { t } = useI18n()
  
     return ( 
@@ -22,119 +13,95 @@ export default function Terms(){
                 <Row>
                     <Col>
                         <PageContent>
+                            <h1>{ t("terms_title") }</h1>
+                            <p className="subtitle">pubclick-marketing.com</p>
+                            
+                            <div className="terms-section">
+                                <h2>1. { t("terms_text4") }</h2>
+                                <p>{ t("terms_text1") }</p>
+                                <p>{ t("terms_text2") }</p>
+                                <p>{ t("terms_text3") }</p>
+                            </div>
 
-                                <h1>{ t("terms_title") }</h1>
-
-                                <p>
-                                { t("terms_text1") }
-                                </p>
-                                <p>
-                                { t("terms_text2") }
-                                </p>
-                                <p>
-                                { t("terms_text3") }
-                                </p>
-                                <b>{ t("terms_text4") }</b>
-                                <p>
-                                { t("terms_text5") }
-                                </p>
-                                <p>
-                                { t("terms_text6") }
-                                </p>
-                                <p>
-                                { t("terms_text7") }
-                                </p>
-
-                                <b>{ t("terms_text8") }</b>
-                                <p>
-                                { t("terms_text9") }
-                                </p>
-                                <p>
-                                { t("terms_text10") }
-                                </p>
-                                <p>
-                                { t("terms_text11") }
-                                </p>
-
-                                <b>{ t("terms_text12") }</b>
-                                <p>
-                                { t("terms_text13") }
-                                </p>
-                                <p>
-                                { t("terms_text14") }
-                                </p>
-                                <p>
-                                { t("terms_text15") }
-                                </p>
-
-                                <b>{ t("terms_text16") }</b>
-                                { t("terms_text17") }
-
-                                <p>
-                                { t("terms_text18") }
-                                </p>
-                                <p>
-                                { t("terms_text19") }
-                                </p>
-                                <p>
-                                { t("terms_text20") }
-                                </p>
-                                <p>
-                                { t("terms_text21") }
-                                </p>
-                                <b>{ t("terms_text22") }</b>
-                                <p>
-                                { t("terms_text23") }
-                                </p>
-                                <p>
-                                { t("terms_text24") }
-                                </p>
-                                <p>
-                                { t("terms_text25") }
-                                </p>
-                                <p>
-                                    { t("terms_text26") }
-                                </p>
+                            <div className="terms-section">
+                                <h2>2. { t("terms_text4") }</h2>
+                                <p>{ t("terms_text5") }</p>
                                 
-                                <b>{ t("terms_text27") }</b>
+                                <p>{ t("terms_text6") }</p>
+                                <ul>
+                                    <li>{ t("terms_text7") }</li>
+                                </ul>
+                                
+                                <p>{ t("terms_text8") }</p>
+                            </div>
+
+                            <div className="terms-section">
+                                <h2>3. { t("terms_text9") }</h2>
+                                <p>{ t("terms_text10") }</p>
+                                
+                                <p>{ t("terms_text11") }</p>
+                                <ul>
+                                    <li>{ t("defamatory_text") || "defamatory, discriminatory, abusive or pornographic content;" }</li>
+                                    <li>{ t("illegal_content_text") || "publication of illegal content or content not aligned with the platform's professional objectives." }</li>
+                                </ul>
+                            </div>
+
+                            <div className="terms-section">
+                                <h2>4. { t("terms_text12") }</h2>
+                                <p>{ t("terms_text13") }</p>
+                                <p>{ t("terms_text14") }</p>
+                                <p>{ t("terms_text15") }</p>
+                            </div>
+
+                            <div className="terms-section">
+                                <h2>5. { t("terms_text16") }</h2>
+                                <p>{ t("terms_text17") }</p>
+                                <p>{ t("terms_text18") }</p>
+                            </div>
+
+                            <div className="terms-section">
+                                <h2>6. { t("terms_text19") }</h2>
+                                <p>{ t("terms_text20") }</p>
+                                <p>{ t("terms_text21") }</p>
+                                <p>{ t("terms_text22") }</p>
+                                <p>{ t("terms_text23") }</p>
+                            </div>
+
+                            <div className="terms-section">
+                                <h2>7. { t("terms_text24") }</h2>
+                                <p>{ t("terms_text25") }</p>
+                                <p>{ t("terms_text26") }</p>
+                            </div>
+
+                            <div className="terms-section">
+                                <h2>8. { t("terms_text27") }</h2>
+                                <p>{ t("terms_text28") }</p>
+                                <p>{ t("terms_text29") }</p>
+                            </div>
+
+                            <div className="terms-section legal-notice">
+                                <h2>{ t("legal_notice") || "Legal Notice" }</h2>
                                 <p>
-                                { t("terms_text28") }
+                                    <strong>{ t("site_publisher") || "Publisher of the site" }</strong><br />
+                                    Luxeo Sàrl<br />
+                                    Route des deux villages 57C<br />
+                                    1806 St-Légier<br />
+                                    Suisse<br />
+                                    IDE : CHE-254.629.393<br />
+                                    Contact@pubclick-marketing.com
                                 </p>
-
-                                <b>{ t("terms_text29") }</b>
                                 <p>
-                                    Publisher of the site
-                                    Wall Street Web
-                                    Chemin J.-Ph.-de-Sauvage 37
-                                    CH-1219 Chatelaine
-                                    Switzerland
-                                    Commercial Register of the Canton of Geneva: CH-660.1.047.015-8
-                                    Company identification number: CHE-155.503.825
+                                    <strong>{ t("hosting_provider") || "Hosting provider" }</strong><br />
+                                    Infomaniak Network SA<br />
+                                    Rue Eugène-Marziano 25,<br />
+                                    1227 Les Acacias – Genève, Suisse<br />
+                                    <a href="https://www.infomaniak.com" target="_blank" rel="noopener noreferrer">www.infomaniak.com</a>
                                 </p>
-                                <b> { t("terms_text30") } </b>
-
-                                <p>
-                                    Jacky Cally
-                                    Wall Street Web
-                                    support@wallstreetweb.net
-                                    +41 78 752 86 56
-                                    Host
-                                    Exoscale
-                                </p>
-
-
-                                <p>
-                                    Boulevard de Grancy 19A
-                                    1006 – Lausanne
-                                    Switzerland
-                                    www.exoscale.com
-                                </p>
-
+                            </div>
                         </PageContent>
                     </Col>
                 </Row>
             </Container>
-
         </ContainerLandpage>
     );
 }
