f8b95d2e60aee5977d4e4514a625c9ce8f5e3ee5
SpiderMan6699 <gregmalyk@gmail.com>
2025-04-09 22:24:15 +0200

    update texts

--- src/components/Landpage/MoreInfos/index.js
+++ src/components/Landpage/MoreInfos/index.js
@@ -24,6 +24,9 @@ export default function MoreInfos(){
         { title: t("homemoreinfos_title1"), text: t("homemoreinfos_text1"),  icon: <AdsClickIcon color="black" sx={{width: 48, height: 48}} /> },
         { title: t("homemoreinfos_title2"), text: t("homemoreinfos_text2"),   icon: <GroupIcon color="black" sx={{width: 48, height: 48}} /> },
         { title: t("homemoreinfos_title3"), text: t("homemoreinfos_text3"),   icon: <GoogleIcon color="black" sx={{width: 48, height: 48}} /> },
+        { title: t("homemoreinfos_title4"), text: t("homemoreinfos_text4"),   icon: <GoogleIcon color="black" sx={{width: 48, height: 48}} /> },
+        { title: t("homemoreinfos_title5"), text: t("homemoreinfos_text5"),   icon: <GoogleIcon color="black" sx={{width: 48, height: 48}} /> },
+        { title: t("homemoreinfos_title6"), text: t("homemoreinfos_text6"),   icon: <GoogleIcon color="black" sx={{width: 48, height: 48}} /> },
     ]
 
     return ( 
